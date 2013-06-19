806399d495d23fd43229058fd476cebd1eb4cfae
Nicolas Bantar <nicobantar87@gmail.com>
2013-06-19 11:49:29 -0300

    Testing Eclipse Plugin.

--- src/TestClass.java
+++ src/TestClass.java
@@ -2,4 +2,13 @@
 //This is just a test class.
 public class TestClass {
 
+	private String testAtribute;
+
+	public String getTestAtribute() {
+		return testAtribute;
+	}
+
+	public void setTestAtribute(String testAtribute) {
+		this.testAtribute = testAtribute;
+	}
 }
